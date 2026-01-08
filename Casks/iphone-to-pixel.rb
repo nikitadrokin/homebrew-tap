98f4f0fcf3bcb24a3f0f33b34b9e06226f13abce
Nikita D <dnawastaken@gmail.com>
2026-01-08 15:16:46 -0800

    fine I'll read it the way it wants

--- Casks/iphone-to-pixel.rb
+++ Casks/iphone-to-pixel.rb
@@ -2,7 +2,7 @@ cask "iphone-to-pixel" do
   version "0.0.4"
   sha256 "a021fee8e26e64037970062f8203ae1ef5d4fe4a02a135a882b03ee8b67c5af8"
 
-  url "https://github.com/nikitadrokin/iphone-to-pixel/releases/download/v#{version}/iphone-to-pixel_#{version}_aarch64.dmg"
+  url "https://github.com/nikitadrokin/iphone-to-pixel/releases/download/v#{version}/iPhone.to.Pixel_#{version}_aarch64.dmg"
   name "iPhone to Pixel"
   desc "Convert iOS media files for Pixel compatibility"
   homepage "https://github.com/nikitadrokin/iphone-to-pixel"
