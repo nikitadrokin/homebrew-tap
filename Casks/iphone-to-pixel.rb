c0b113d108b000cba8510eb14beeddebbc3071e7
Nikita D <dnawastaken@gmail.com>
2026-01-08 15:19:15 -0800

    corrected hash

--- Casks/iphone-to-pixel.rb
+++ Casks/iphone-to-pixel.rb
@@ -1,6 +1,6 @@
 cask "iphone-to-pixel" do
   version "0.0.4"
-  sha256 "a021fee8e26e64037970062f8203ae1ef5d4fe4a02a135a882b03ee8b67c5af8"
+  sha256 "8411ae8cbbc9730e5aa9a4643a3bbcaa2fb69112a56f0ceb17a0f85a8c9cf4fa"
 
   url "https://github.com/nikitadrokin/iphone-to-pixel/releases/download/v#{version}/iPhone.to.Pixel_#{version}_aarch64.dmg"
   name "iPhone to Pixel"
