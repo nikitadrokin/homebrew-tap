f044e12ebca0b87357b3757db777baa897d5c959
Nikita D <dnawastaken@gmail.com>
2026-03-20 13:09:24 -0700

    Release v0.0.10

--- Casks/iphone-to-pixel.rb
+++ Casks/iphone-to-pixel.rb
@@ -1,6 +1,6 @@
 cask "iphone-to-pixel" do
-  version "0.0.9"
-  sha256 "94063d30ef94bc7fb5613ae9416b6dce5d70c7193f639b03b83b6b994f81b60e"
+  version "0.0.10"
+  sha256 "6e2993ce47ac0facf2c88b47e6f6df1012561f781d1e0a2de107e19b2b42b7c4"
 
   url "https://github.com/nikitadrokin/iphone-to-pixel/releases/download/v#{version}/iPhone.to.Pixel_#{version}_aarch64.dmg"
   name "iPhone to Pixel"
