84e29f9ce03bd9de970a0b727e83c05a59933a9a
Nikita D <dnawastaken@gmail.com>
2026-07-05 23:18:53 -0700

    Release v0.0.18

--- Casks/photo-bridge.rb
+++ Casks/photo-bridge.rb
@@ -1,6 +1,6 @@
 cask "photo-bridge" do
-  version "0.0.17"
-  sha256 "5c6769eaacaa4daa8bc0b041e20adaad4c157da40c97f4e2d5afd1b8aa7f7ba2"
+  version "0.0.18"
+  sha256 "5dfd48b18ac46016cc6426f47521c0609c89951a421b90665abd773142585689"
 
   url "https://github.com/nikitadrokin/photo-bridge/releases/download/v#{version}/Photo.Bridge_#{version}_aarch64.dmg"
   name "Photo Bridge"
